693127a158a9d90877fa54e40c2008d330fc60d3
henriquewaisman <100635335+henriquewaisman@users.noreply.github.com>
2025-03-07 13:42:23 -0300

    Create Produto.cs

---
+++ dotnetLINQbasics/models/Produto.cs
@@ -0,0 +1,11 @@
+class Produto
+{
+    public string Nome { get; set; }
+    public double Preco { get; set; }
+
+    public Produto(string nome, double preco)
+    {
+        Nome = nome;
+        Preco = preco;
+    }
+}
